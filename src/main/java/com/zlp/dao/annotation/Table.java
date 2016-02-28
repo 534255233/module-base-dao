b93d3bbce9812b5dc6411265778c324309d20da1
zhoulongpeng <1507637678@qq.com>
2016-02-28 10:54:20 +0800

    add table annotation

--- src/main/java/com/zlp/dao/annotation/Table.java
+++ src/main/java/com/zlp/dao/annotation/Table.java
@@ -8,12 +8,28 @@ import java.lang.annotation.Target;
 /**
  * 描述：mongodb表名注解
  * @author zhoulongpeng
+ * @date   2016-02-17
+ * 
+ * @Target   : 说明了Annotation所修饰的对象范围
+ * 取值(ElementType)有：
+ * 1.CONSTRUCTOR:用于描述构造器
+ * 2.FIELD:用于描述域
+ * 3.LOCAL_VARIABLE:用于描述局部变量
+ * 4.METHOD:用于描述方法
+ * 5.PACKAGE:用于描述包
+ * 6.PARAMETER:用于描述参数
+ * 7.TYPE:用于描述类、接口(包括注解类型) 或enum声明
+ * @Retention: 该Annotation被保留的时间长短
+ * 取值（RetentionPoicy）有：
+ * 1.SOURCE:在源文件中有效（即源文件保留）
+ * 2.CLASS:在class文件中有效（即class保留）
+ * 3.RUNTIME:在运行时有效（即运行时保留）
  *
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Table {
 
-	public String value() default "";
+	public String value() default "className";
 	
 }
